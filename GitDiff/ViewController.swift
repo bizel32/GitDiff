b3cdda2d5a87b5ac1069714f60662087ec04caa4
Jonathan Brower <bizel32@Browers-Macbook-Pro.local>
2017-06-21 10:22:30 -0700

    Initial Commit

---
+++ GitDiff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDiff
+//
+//  Created by Jonathan Brower on 6/21/17.
+//  Copyright © 2017 Jonathan Brower. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
